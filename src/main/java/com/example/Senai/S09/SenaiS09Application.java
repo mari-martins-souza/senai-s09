ae0c58f065dcf2d624a428b1c21f3e5321b46975
Mariana Martins <marih_ms@hotmail.com>
2024-08-11 17:32:08 -0300

    first commit

---
+++ src/main/java/com/example/Senai/S09/SenaiS09Application.java
@@ -0,0 +1,13 @@
+package com.example.Senai.S09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiS09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiS09Application.class, args);
+	}
+
+}
